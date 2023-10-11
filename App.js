4961ef3c673b67c0cf74424f9c7a31b78f8f3153
yishanhsieh <47290910+yishanhsieh@users.noreply.github.com>
2023-10-10 23:20:00 -0400

    Add navigator to move between screens

--- App.js
+++ App.js
@@ -1,77 +1,23 @@
 import * as React from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Button,
-  FlatList,
-  Image,
-  SafeAreaView,
-} from "react-native";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import PlaySound from "./components/PlaySound";
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import RandomOption from "./components/RandomOption";
+import EntryPoint from "./components/EntryPoint";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [album, SetAlbum] = useState([]);
-
-  const getAlbum = async () => {
-    try {
-      const response = await axios({
-        method: "GET",
-        url: "https://spotify23.p.rapidapi.com/playlist_tracks/",
-        params: {
-          id: "2siMQsSv15yXBDdjmUSfJX",
-          offset: "0",
-          limit: "10",
-        },
-        headers: {
-          "X-RapidAPI-Key":
-            "64ce64b6e5msh65817e0e1e29fc9p1afae2jsnb989f18d4c59",
-          "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
-        },
-      });
-      const tracks = response.data.items;
-      SetAlbum(tracks);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
-  useEffect(() => {
-    getAlbum();
-  }, []);
-
-  const renderItem = ({ item }) => {
-    return (
-      <Image
-        style={styles.image}
-        source={{ uri: item.track.album.images[0].url }}
-      ></Image>
-    );
-  };
-
   return (
-    <View style={styles.container}>
-      <SafeAreaView>
-        <PlaySound />
-        <FlatList data={album} renderItem={renderItem} />
-      </SafeAreaView>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="EntryPoint">
+        <Stack.Screen name="RandomOption" component={RandomOption} />
+        <Stack.Screen
+          name="EntryPoint"
+          component={EntryPoint}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-    padding: 10,
-    marginTop: 30,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    margin: 10,
-    justifyContent: "center",
-  },
-});
